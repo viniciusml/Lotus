cc8ed9e5edf0a1183c2a7496a662cd8e9af6a5d2
Vinicius Leal <viniciusmoreira.leal@thetrainline.com>
2024-08-10 23:06:00 +0100

    Implement async integration tests

--- Lotus/Tests/IntegrationTests/AsyncIntegrationTests.swift
+++ Lotus/Tests/IntegrationTests/AsyncIntegrationTests.swift
@@ -3,9 +3,24 @@ import Lotus
 
 final class AsyncIntegrationTests: XCTestCase {
     
+    private var notificationCenter: NotificationCenter!
+    
+    override func setUp() {
+        super.setUp()
+        
+        notificationCenter = NotificationCenter()
+    }
+    
+    override func tearDown() {
+        
+        notificationCenter = nil
+        super.tearDown()
+    }
+    
     // If the network is initially available, the given closure is invoked
     func testAsyncOperationExecutedWithConnectionAvailable() {
-        let sut = NetworkOperationPerformer(networkMonitor: NetworkMonitorStub(connection: true))
+        let networkMonitor = NetworkMonitorStub(connection: true, notificationCenter: notificationCenter)
+        let sut = NetworkOperationPerformer(networkMonitor: networkMonitor, notificationCenter: notificationCenter)
         let exp = expectation(description: #function)
         
         sut.performNetworkOperation(using: {
@@ -15,9 +30,22 @@ final class AsyncIntegrationTests: XCTestCase {
         wait(for: [exp], timeout: 0.1)
     }
     
+    func testAsyncOperationExecutedWithConnectionAvailableAsync() async {
+        let networkMonitor = NetworkMonitorStub(connection: true, notificationCenter: notificationCenter)
+        let sut = NetworkOperationPerformer(networkMonitor: networkMonitor, notificationCenter: notificationCenter)
+        let exp = expectation(description: #function)
+        
+        await sut.perform(withinSeconds: 3) {
+            exp.fulfill()
+        }
+        
+        await fulfillment(of: [exp], timeout: 0.1)
+    }
+    
     // If no network is available, the given closure is not invoked
     func testAsyncOperationNotExecutedWithNoConnectionAvailable() {
-        let sut = NetworkOperationPerformer(networkMonitor: NetworkMonitorStub(connection: false))
+        let networkMonitor = NetworkMonitorStub(connection: false, notificationCenter: notificationCenter)
+        let sut = NetworkOperationPerformer(networkMonitor: networkMonitor, notificationCenter: notificationCenter)
         let exp = expectation(description: #function).inverted()
         
         sut.performNetworkOperation(using: {
@@ -27,10 +55,22 @@ final class AsyncIntegrationTests: XCTestCase {
         wait(for: [exp], timeout: 0.1)
     }
     
+    func testAsyncOperationNotExecutedWithNoConnectionAvailableAsync() async {
+        let networkMonitor = NetworkMonitorStub(connection: false, notificationCenter: notificationCenter)
+        let sut = NetworkOperationPerformer(networkMonitor: networkMonitor, notificationCenter: notificationCenter)
+        let exp = expectation(description: #function).inverted()
+        
+        await sut.perform(withinSeconds: 3) {
+            exp.fulfill()
+        }
+        
+        await fulfillment(of: [exp], timeout: 0.1)
+    }
+    
     // If the network is initially not available but becomes available within the given timeout duration, the given closure is invoked
     func testAsyncOperationExecutedWithNoConnectionAvailableButResumedInTime() {
-        let networkMonitor = NetworkMonitorStub(connection: false)
-        let sut = NetworkOperationPerformer(networkMonitor: networkMonitor)
+        let networkMonitor = NetworkMonitorStub(connection: false, notificationCenter: notificationCenter)
+        let sut = NetworkOperationPerformer(networkMonitor: networkMonitor, notificationCenter: notificationCenter)
         let exp = expectation(description: #function)
         
         sut.performNetworkOperation(using: {
@@ -41,10 +81,23 @@ final class AsyncIntegrationTests: XCTestCase {
         wait(for: [exp], timeout: 0.2)
     }
     
+    func testAsyncOperationExecutedWithNoConnectionAvailableButResumedInTimeAsync() async {
+        let networkMonitor = NetworkMonitorStub(connection: false, notificationCenter: notificationCenter)
+        let sut = NetworkOperationPerformer(networkMonitor: networkMonitor, notificationCenter: notificationCenter)
+        let exp = expectation(description: #function)
+        
+        await sut.perform(withinSeconds: 0.1) {
+            exp.fulfill()
+        }
+        networkMonitor.flipConnectionStubbedStatusAndSimulateNotification()
+        
+        await fulfillment(of: [exp], timeout: 0.2)
+    }
+    
     // If the network is initially not available and becomes available only after the given timeout duration, the given closure is not invoked
     func testAsyncOperationNotExecutedWithNoConnectionAvailableButNotResumedInTime() {
-        let networkMonitor = NetworkMonitorStub(connection: false)
-        let sut = NetworkOperationPerformer(networkMonitor: networkMonitor)
+        let networkMonitor = NetworkMonitorStub(connection: false, notificationCenter: notificationCenter)
+        let sut = NetworkOperationPerformer(networkMonitor: networkMonitor, notificationCenter: notificationCenter)
         let exp = expectation(description: #function).inverted()
         
         sut.performNetworkOperation(using: {
@@ -56,16 +109,33 @@ final class AsyncIntegrationTests: XCTestCase {
         
         wait(for: [exp], timeout: 0.1)
     }
+    
+    func testAsyncOperationNotExecutedWithNoConnectionAvailableButNotResumedInTimeAsync() async {
+        let networkMonitor = NetworkMonitorStub(connection: false, notificationCenter: notificationCenter)
+        let sut = NetworkOperationPerformer(networkMonitor: networkMonitor, notificationCenter: notificationCenter)
+        let exp = expectation(description: #function).inverted()
+        
+        await sut.perform(withinSeconds: 0.1) {
+            exp.fulfill()
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2, execute: {
+            networkMonitor.flipConnectionStubbedStatusAndSimulateNotification()
+        })
+        
+        await fulfillment(of: [exp], timeout: 0.1)
+    }
 }
 
 private extension AsyncIntegrationTests {
     
     final class NetworkMonitorStub: NetworkMonitoring {
         
+        private let notificationCenter: NotificationCenter
         private var stubbedHasInternetConnection: Bool
         
-        init(connection stubbedHasInternetConnection: Bool) {
+        init(connection stubbedHasInternetConnection: Bool, notificationCenter: NotificationCenter) {
             self.stubbedHasInternetConnection = stubbedHasInternetConnection
+            self.notificationCenter = notificationCenter
         }
         
         func hasInternetConnection() -> Bool {
@@ -74,7 +144,7 @@ private extension AsyncIntegrationTests {
         
         func flipConnectionStubbedStatusAndSimulateNotification() {
             stubbedHasInternetConnection = true
-            NotificationCenter.default.post(
+            notificationCenter.post(
                 name: Notification.Name("NetworkStatusDidChange"),
                 object: nil,
                 userInfo: ["connected": self.hasInternetConnection()]
