ab16ccb8582dc86ecf63c5d0016c25b2d9b6ced5
Vinicius Leal <viniciusmoreira.leal@thetrainline.com>
2024-08-11 11:06:38 +0100

    make package available for iPhone

--- Lotus/Package.swift
+++ Lotus/Package.swift
@@ -5,7 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "Lotus",
-    platforms: [.macOS(.v14)],
+    platforms: [
+        .macOS(.v14),
+        .iOS(.v15)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
